4abca8fdb0820ac2604ee3f6183f0d986a76d8d2
zhensg123 <xyqblog@163.com>
2022-08-09 22:09:23 +0800

    提交

--- src/main/java/object/JavaObject.java
+++ src/main/java/object/JavaObject.java
@@ -5,8 +5,18 @@ public class JavaObject {
     int month;
     double weight;
 
+    public  JavaObject(){
+       System.out.println("这是输出项目");
+    }
+    public  JavaObject(String name, int month, double weight){
+        this();
+      this.name = name;
+      this.month = month;
+      this.weight = weight;
+    }
     public void eat(){
-        System.out.println("吃鱼");
+        run();
+        System.out.println(this.weight);
     }
     public void run(){
         System.out.println("小猫快跑");
--- src/main/java/test/javaTest.java
+++ src/main/java/test/javaTest.java
@@ -4,7 +4,7 @@ import object.JavaObject;
 
 public class javaTest {
     public static void main(String[] args) {
-        JavaObject cat = new JavaObject();
+        JavaObject cat = new JavaObject("22", 1, 1.1);
         JavaObject tt = cat;
         cat.eat();
         cat.run();
